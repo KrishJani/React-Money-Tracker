e85bbfed4d75f076a4e18c8823e61090dde92c77
krish <krishjani01@gmail.com>
2024-03-17 12:05:09 +0530

    adding frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<spam>.00</spam></h1>
+      <div>
+        <form>
+          <div className = "basic">
+            <input type = "text" placeholder = {'+200 samsung TV'}></input>
+            <input type = "datetime-local"></input>
+          </div>
+          <div className = "description">
+            <input type = "text" placeholder = {'Description'}></input>
+          </div>
+          <button type = "submit"> Add new transaction</button>
+        </form>
+
+        <div className = "transactions">
+          <div className = "transaction">
+            <div className = "left">
+              <div className = "name">New Samsung TV</div>
+              <div className = "description">It was time for a new TV</div>
+            </div>
+            <div className = "right">
+              <div className = "price red">-$500</div>
+              <div className = "dateTime">25-05-2024 18:30</div>
+            </div>
+          </div>
+
+          <div className = "transaction">
+            <div className = "left">
+              <div className = "name">Freelance project</div>
+              <div className = "description">It was time for a new TV</div>
+            </div>
+            <div className = "right">
+              <div className = "price green">+$700</div>
+              <div className = "dateTime">25-05-2024 18:30</div>
+            </div>
+          </div>
+
+          <div className = "transaction">
+            <div className = "left">
+              <div className = "name">IPhone</div>
+              <div className = "description">It was time for a new TV</div>
+            </div>
+            <div className = "right">
+              <div className = "price red">-$900</div>
+              <div className = "dateTime">25-05-2024 18:30</div>
+            </div>
+          </div>
+
+          <div className = "transaction">
+            <div className = "left">
+              <div className = "name">New Samsung TV</div>
+              <div className = "description">It was time for a new TV</div>
+            </div>
+            <div className = "right">
+              <div className = "price green">$500</div>
+              <div className = "dateTime">25-05-2024 18:30</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    
   );
 }
 
